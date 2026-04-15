331237cd9c11442883f18043ca4a6e9d04237592
PaulArkhet <spkim0921@gmail.com>
2026-04-15 13:30:32 -0700

    feat: build get users api route

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"wealthintelligence/db"
+	"wealthintelligence/models"
+
 	"github.com/go-chi/chi/v5"
 )
 
@@ -11,8 +14,16 @@ func UsersRouter() chi.Router {
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		users := []models.User{}
+
+		err := db.DB.Select(&users, "SELECT * FROM users")
+		if err != nil {
+			http.Error(w, "Failed to fetch users", http.StatusInternalServerError)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"message": "get all users"})
+		json.NewEncoder(w).Encode(users)
 	})
 
 	return r
